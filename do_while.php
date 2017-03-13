0568effbe1ec24dd45968c6129c16ce3c9a4446b
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-13 16:22:34 -0500

    Added all three exercises and commented out the first 2.

--- do_while.php
+++ do_while.php
@@ -1,4 +1,25 @@
-<?php 
+<?php
+// -----------------------------
+// // First Exercise
+// -----------------------------
+// 	$number = 2;
+
+// 	do {
+// 		echo $number . PHP_EOL;
+// 		$number += 2;
+// 	} while ($number <= 100);
+// -----------------------------
+// // Second Exercise
+// -----------------------------
+// 	$number = 100;
+
+// 	do {
+// 		echo $number . PHP_EOL;
+// 		$number -= 5;
+// 	} while ($number >= -10);
+// -----------------------------
+// Third Exercise
+// -----------------------------
 	$number = 2;
 
 	do {
