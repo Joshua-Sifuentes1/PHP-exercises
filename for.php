367c6315b343bd51db996ce3f73c859d82f7b25c
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-15 10:56:16 -0500

    Added do while loop for ending value output. Will prompt erorr message if input is not numeric.

--- for.php
+++ for.php
@@ -1,17 +1,36 @@
 <?php  
 // prompt user for starting number
 
-fwrite(STDOUT, 'Enter a starting value: ');
-$start = trim(fgets(STDIN));
+// added do-while loop if user does not enter a numeric value;
+do{	
+	fwrite(STDOUT, 'Enter a starting value: ');
+	$start = trim(fgets(STDIN));
+	if (!is_numeric($start)){
+		fwrite(STDOUT, 'Invalid input, please enter an integer value' . PHP_EOL);
+	}
+} while(!is_numeric($start));
+
 
 // prompt the user for ending number
 
-fwrite(STDOUT, 'Enter a starting value: ');
-$end = trim(fgets(STDIN));
+// loop for invalid input
+do{	
+	fwrite(STDOUT, 'Enter a ending value: ');
+	$end = trim(fgets(STDIN));
+	if (!is_numeric($end)){
+		fwrite(STDOUT, 'Invalid input, please enter an integer value' . PHP_EOL);
+	}
+} while(!is_numeric($end));
 
 // Allow user to select the increment the numbers are displayed by.
 fwrite(STDOUT, 'Select increment: ');
 $increment = trim(fgets(STDIN));
+
+// defaulting increment to 1 if value is not numeric.
+
+if (!is_numeric($increment)){
+	$increment = 1;
+} 
 
 // Display all the numbers from the starting number and the ending number in a For loop
 
