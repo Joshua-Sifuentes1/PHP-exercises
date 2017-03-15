f2f418df6ec7939fcb7b81b18efefb7da83fb68c
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-15 16:43:03 -0500

    Added foreach loop iterating through each value and outputs its type using gettype

--- foreach.php
+++ foreach.php
@@ -2,5 +2,9 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
+// construct a forEach loop that iterates through each value and outputs it's type integer, float, boolean, array, null, or string
 
+foreach ($things as $thing){
+	echo gettype($thing) . PHP_EOL;
+}
 ?>
