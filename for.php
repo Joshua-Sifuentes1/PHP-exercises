17a2e102067c1f00d5590be80e91a26cd6621231
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-15 10:38:53 -0500

    Added fwrite to allow user to select increment value. Changed for loop to add by the increment value inputted by the user.

--- for.php
+++ for.php
@@ -9,9 +9,13 @@ $start = trim(fgets(STDIN));
 fwrite(STDOUT, 'Enter a starting value: ');
 $end = trim(fgets(STDIN));
 
+// Allow user to select the increment the numbers are displayed by.
+fwrite(STDOUT, 'Select increment: ');
+$increment = trim(fgets(STDIN));
+
 // Display all the numbers from the starting number and the ending number in a For loop
 
-for ($i = $start; $i <= $end; $i++) {
+for ($i = $start; $i <= $end; $i += $increment) {
 	echo $i . PHP_EOL;
 }
 
