dd6de9a33d22ad0be452f37d73cd4af8f1c83f08
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-15 11:14:13 -0500

    Added if statement with a condition if a value is divisible by both 3 and 5 it will echo FizzBuzz.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -2,6 +2,11 @@
 // For loop that will echo numbers 1-100.
 
 for ($i = 1; $i <= 100; $i++){
-	echo $i . PHP_EOL;
+	// create if statement that will replace numbers divisible by 3 and 5 with FizzBuzz using modulus.
+	if (($i % 5 == 0) && ($i % 3 == 0)){
+		echo "FizzBuzz" . PHP_EOL;
+	} else{
+		echo $i . PHP_EOL;
+	}
 }
 
