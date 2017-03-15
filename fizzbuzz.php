64351c6868b9784c2f37d988bd00b693429d0d61
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-15 11:18:02 -0500

    Created elseif statement for values only divisible by 3 to display Fizz and another elseif statement for values only divisible by 5 to display Buzz. If none of those conditions are met, the last else statement will echo the  value for the counter.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -5,6 +5,10 @@ for ($i = 1; $i <= 100; $i++){
 	// create if statement that will replace numbers divisible by 3 and 5 with FizzBuzz using modulus.
 	if (($i % 5 == 0) && ($i % 3 == 0)){
 		echo "FizzBuzz" . PHP_EOL;
+	} elseif ($i % 3 == 0) {
+		echo "Fizz" . PHP_EOL;
+	} elseif ($i % 5 == 0) {
+		echo "Buzz" . PHP_EOL;
 	} else{
 		echo $i . PHP_EOL;
 	}
