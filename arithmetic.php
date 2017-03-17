fea9ef68d8c15c14dfd85a19b1a96c92cbdb55c9
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-17 16:17:53 -0500

    Added error function. Organized code and formatted for cleaner look. Moved PHP_EOL from echos at bottom of page to returns in functions.

--- arithmetic.php
+++ arithmetic.php
@@ -2,31 +2,57 @@
 
 function add($a, $b)
 {
-	return $a + $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a + $b . PHP_EOL;
+	} else {
+		echo errorMessage($a, $b);
+	}
 }
 
 function subtract($a, $b)
 {
-	return $a - $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a - $b . PHP_EOL;
+	} else {
+		echo errorMessage($a, $b);
+	}
 }
 
 function multiply($a, $b)
 {
-	return $a * $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a * $b . PHP_EOL;
+	} else {
+		echo errorMessage($a, $b);
+	}
 }
 
 function divide($a, $b)
 {
-	return $a / $b;
+	if ($b == 0 ) {
+		return "ERROR: Cannot divide by ZERO. Values input: $a and $b" . PHP_EOL;
+	} elseif (is_numeric($a) && is_numeric($b)) {
+		return $a / $b . PHP_EOL;
+	} else {
+		echo errorMessage($a, $b);
+	}
 }
 
 function modulus($a, $b)
 {
-	return $a % $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a % $b . PHP_EOL;
+	} else {
+		echo errorMessage($a, $b);
+	}
 }
 
-echo add(4, 2) . PHP_EOL;
-echo subtract(4, 2) . PHP_EOL;
-echo multiply(4, 2) . PHP_EOL;
-echo divide(4, 2) . PHP_EOL;
-echo modulus(3, 2) . PHP_EOL;
+function errorMessage($a, $b){
+	return "ERROR: Both arguments must be numeric. Values input: $a and $b" . PHP_EOL;
+}
+
+echo add(4, 2);
+echo subtract("h", 2);
+echo multiply(4, 2);
+echo divide(4, 0);
+echo modulus(3, "b");
