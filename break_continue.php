bebe41455c415a5a440a7fcee4fca483cfd46ef2
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-17 10:59:54 -0500

    Created for loop that counts from 1 to 100 using break to stop once the value hits 10.

--- break_continue.php
+++ break_continue.php
@@ -2,7 +2,7 @@
 
 // Create a for loop that creates a counter from 1 to 100 but only displaying even numbers
 for ($i = 1; $i <= 100; $i++){
-	
+
 	if ($i % 2 == 1){
 		continue;
 	}
@@ -10,4 +10,14 @@ for ($i = 1; $i <= 100; $i++){
 	echo $i . PHP_EOL;
 }
 
-// Creater a for loop from 1 to 100 but stopping after number 10
+echo PHP_EOL;
+
+// Create a for loop from 1 to 100 but stopping after number 10
+for ($i = 1; $i <= 100; $i++){
+
+	if ($i > 10){
+		break;
+	}
+
+	echo $i . PHP_EOL;
+}
