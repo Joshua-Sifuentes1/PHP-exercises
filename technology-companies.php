729af012d7506889ebc037e6dfda0f8f2da4fb16
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-20 11:20:36 -0500

    completed exercise.

--- technology-companies.php
+++ technology-companies.php
@@ -1,48 +1,65 @@
 <?php
 
 $companies = [
-    'Sun Microsystems' => [
-        'Vinod Khosla',
-        'Bill Joy',
-        'Andy Bechtolsheim',
-        'Scott McNealy'
-    ],
-    'Silicon Graphics' => [
-        'Jim Clark',
-        'Ed McCracken'
-    ],
-    'Cray' => [
-        'William Norris',
-        'Seymour Cray'
-    ],
-    'NeXT' => [
-        'Steve Jobs',
-        'Avie Tevanian',
-        'Joanna Hoffman',
-        'Bud Tribble',
-        'Susan Kare'
-    ],
-    'Acorn Computers' => [
-        'Steve Furber',
-        'Sophie Wilson',
-        'Hermann Hauser',
-        'Jim Mitchell'
-    ],
-    'MIPS Technologies' => [
-        'Skip Stritter',
-        'John L. Hennessy'
-    ],
-    'Commodore' => [
-        'Yash Terakura',
-        'Bob Russell',
-        'Bob Yannes',
-        'David A. Ziembicki',
-        'Jay Miner'
-    ],
-    'Be Inc' => [
-        'Steve Sakoman',
-        'Jean-Louis Gassée'
-    ]
+	'Sun Microsystems' => [
+		'Vinod Khosla',
+		'Bill Joy',
+		'Andy Bechtolsheim',
+		'Scott McNealy'
+	],
+	'Silicon Graphics' => [
+		'Jim Clark',
+		'Ed McCracken'
+	],
+	'Cray' => [
+		'William Norris',
+		'Seymour Cray'
+	],
+	'NeXT' => [
+		'Steve Jobs',
+		'Avie Tevanian',
+		'Joanna Hoffman',
+		'Bud Tribble',
+		'Susan Kare'
+	],
+	'Acorn Computers' => [
+		'Steve Furber',
+		'Sophie Wilson',
+		'Hermann Hauser',
+		'Jim Mitchell'
+	],
+	'MIPS Technologies' => [
+		'Skip Stritter',
+		'John L. Hennessy'
+	],
+	'Commodore' => [
+		'Yash Terakura',
+		'Bob Russell',
+		'Bob Yannes',
+		'David A. Ziembicki',
+		'Jay Miner'
+	],
+	'Be Inc' => [
+		'Steve Sakoman',
+		'Jean-Louis Gassée'
+	]
 ];
 
-print_r($companies);
+// print_r($companies);
+
+ksort($companies);
+// print_r($companies);
+
+// Sort the people in each company alphabetically. foreach loop
+
+foreach ($companies as $company => $people) {
+	asort($people);
+	$companies[$company] = $people;
+}
+
+// var_dump($companies);
+
+arsort($companies);
+
+var_dump($companies);
+
