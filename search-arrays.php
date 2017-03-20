edee52fb360b1243e0bffa8e68490e6b29b76183
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-20 14:13:12 -0500

    Exercises completed. Created 2 functions to search array for value and compare values for 2 arrays.

---
+++ search-arrays.php
@@ -0,0 +1,39 @@
+<?php
+
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+$nameSearch = 'Bob';
+
+// Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.
+
+function search($name, $array){
+
+	$result = array_search($name, $array);
+
+	if($result !== false) {
+		return "True" . PHP_EOL;
+	} else {
+		return "False" . PHP_EOL;
+	}
+}
+
+echo search($nameSearch, $names);
+
+// Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
+
+function compare($firstArray, $secondArray){
+	$i = 0;
+	foreach($firstArray as $name) {
+
+		$result = array_search($name, $secondArray);
+		
+		if ($result !== false){
+			$i++;
+		}
+	}
+	return $i . PHP_EOL;
+}
+echo compare($names, $compare);
+
