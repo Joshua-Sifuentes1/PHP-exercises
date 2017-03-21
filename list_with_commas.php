d63694960f565816963323ac4edd0a3619d879fc
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-21 11:00:00 -0500

    completed exercise. Added instructor examples to code.

--- list_with_commas.php
+++ list_with_commas.php
@@ -3,21 +3,64 @@
 // Converts array into list n1, n2, ..., and n3
 function humanizedList($array) {
 	$lastIndex = array_pop($array);
-	$lastIndex = " and$lastIndex";
+	$lastIndex = "and $lastIndex";
 	array_push($array, $lastIndex);
-	return implode(",", $array);
+	return implode(", ", $array);
 
 }
 
+// instructor example
+// -------------------------------------------------------------------------
+
+function humanizedListSort ($array, $sortNames = false)
+{
+	if ($sortNames == true) {
+		sort($array);
+	}
+
+	$output = "";
+	$lastValue = array_pop($array);
+	$output = implode(", ", $array);
+	$output .= ", and $lastValue";
+
+	return $output;
+}
+
+function humanizedFirstNameList ($array) {
+	$output = "";
+
+	//eliminate Last name
+
+	$names = implode(" ", $array);
+	$namesArray = explode(" ", $names);
+	$firstNamesArray = [];
+	for ($i = 0; $i < count($namesArray); $i += 2) {
+		array_push($firstNamesArray, $namesArray[$i]);
+	}
+	if ($sortNames == true) {
+		sort($firstNamesArray);
+	}
+
+	$lastValue = array_pop($firstNamesArray);
+	$output = implode(", ", $firstNamesArray);
+	$output .= ", and $lastValue";
+
+	return $output;
+}
+
+// -------------------------------------------------------------------------
+
 // List of famous peeps
-$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark, Joshua Sifuentes';
 
 // TODO: Convert the string into an array
-$physicistsArray = explode(",", $physicistsString);
+$physicistsArray = explode(", ", $physicistsString);
 
+sort($physicistsArray);
 
 // Humanize that list
 $famousFakePhysicists = humanizedList($physicistsArray);
+
 
 // Output sentence
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." .PHP_EOL;
