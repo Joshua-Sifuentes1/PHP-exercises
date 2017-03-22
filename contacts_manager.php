b5fa85228348a6918e1587850c5da07f32328b3b
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-22 14:39:04 -0500

    Added do while loops to new contact function when the correct information is not entered in the right format.

--- contacts_manager.php
+++ contacts_manager.php
@@ -14,10 +14,15 @@ function mainMenu()
 	fwrite(STDOUT, "4.) Delete a contact." . PHP_EOL);
 	fwrite(STDOUT, "5.) Exit" . PHP_EOL);
 
+	optionSelected();
+}
+
+function optionSelected()
+{
 	do {
 		fwrite(STDOUT, "Select an option (1-5): ");
-		$userInput = trim(fgets(STDIN));
-	} while (!is_numeric($userInput) || ($userInput > 5) || ($userInput == 0));
+		$userInput = trim(fgets(STDIN) . PHP_EOL);
+	} while (!is_numeric($userInput) || ($userInput > 5) || ($userInput <= 0));
 
 	switch ($userInput) {
 	case 1:
@@ -42,15 +47,68 @@ function mainMenu()
 
 // The user interface for your application must include a main menu like the following, where the user will need to enter a number between 1 and 5. ```
 
+// function to add contacts
+
 // View contacts.
 function viewContacts()
 {
-	echo "Hello!" . PHP_EOL;
+	clearstatcache();
+	$filename = "contacts.txt";
+
+	$handle = fopen($filename, 'r');
+	$contents = fread($handle, filesize($filename));
+	fclose($handle);
+	echo "-----------------------" . PHP_EOL;
+	echo "Name	| Phone Number" . PHP_EOL;
+	echo "-----------------------" . PHP_EOL;
+	echo $contents . PHP_EOL;
+
+	mainMenu();
 }
 // Add a new contact.
 function newContact()
 {
-	echo "blah" . PHP_EOL;
+	do {
+		$filename = "contacts.txt";
+
+		$handle = fopen($filename, "a");
+
+		do {
+
+		fwrite(STDOUT, "Enter the first name:" . PHP_EOL);
+		$firstName = trim(fgets(STDIN));
+
+		} while (!is_string($firstName) || $firstName == "");
+
+		do {
+
+		fwrite(STDOUT, "Enter the last name:" . PHP_EOL);
+		$lastName = trim(fgets(STDIN));
+
+		} while (!is_string($lastName) || $lastName == "");
+
+		do{
+			fwrite(STDOUT, "Enter the phone number:" . PHP_EOL);
+			$phoneNumber = trim(fgets(STDIN));
+
+			if (!is_numeric($phoneNumber)) {
+				echo "Please enter numbers only." . PHP_EOL;
+			} elseif (strlen($phoneNumber) < 10 || strlen($phoneNumber) > 10) {
+				echo "Please enter the the correct 10 digit number." . PHP_EOL;
+			}
+		} while (!is_numeric($phoneNumber) || strlen($phoneNumber) < 10 || strlen($phoneNumber) > 10);
+
+		fwrite(STDOUT, "Is the information correct (y/n)?" . PHP_EOL);
+		fwrite(STDOUT, "$firstName $lastName|$phoneNumber" . PHP_EOL);
+		$correctInfo = trim(fgets(STDIN));
+	} while ($correctInfo == "N" || $correctInfo == "n");
+
+	fwrite($handle, "$firstName $lastName|$phoneNumber" . PHP_EOL);
+
+	fclose($handle);
+
+	mainMenu();
+	
 }
 
 // Search a contact by name.
